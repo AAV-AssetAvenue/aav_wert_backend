d8ef1a82bcb1416db86502dbfc35ec01eccf1c92
Asadullah khan <asadmuhammad427@gmail.com>
2025-04-04 16:05:12 +0500

    fixed build deployment error

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -6,10 +6,15 @@ import { Order, OrderSchema } from "src/mongoose/schemas/order.schema";
 import { AuthModule } from "src/auth/auth.module";
 import { HttpModule } from "@nestjs/axios";
 import { CryptoOrder,CryptoOrderSchema} from "src/mongoose/schemas/cryptoOrder.schema";
+import { Commission, CommissionSchema } from "src/mongoose/schemas/commission.schema";
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema },{name: CryptoOrder.name, schema: CryptoOrderSchema }]),
+    MongooseModule.forFeature([
+      { name: Order.name, schema: OrderSchema },
+      {name: CryptoOrder.name, schema: CryptoOrderSchema },
+      { name: Commission.name, schema: CommissionSchema },
+    ]),      
     AuthModule,
     HttpModule,
   ],
